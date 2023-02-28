bdf23968146ac8a32596794d7f261064e59ac622
arslan456 <muhammadarslanjabbar@gmail.com>
2023-02-28 17:07:35 +0500

    'form-design-fixed'

--- pages/contactUs/index.jsx
+++ pages/contactUs/index.jsx
@@ -1,103 +1,171 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./shipping.module.css";
-import { createStyles, makeStyles } from "@mui/styles"
-import { Container, Grid, Typography, Card, Box, Button, MenuItem, FormGroup, FormControl } from "@mui/material";
-import TextField from "@mui/material/TextField";
+import { createStyles, makeStyles } from "@mui/styles";
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  Box,
+  Button,
+  MenuItem,
+  FormGroup,
+  FormControl,
+  Modal,
+  Alert,
+} from "@mui/material";
 import emailjs from "@emailjs/browser";
 import { useRouter } from "next/router";
+import { LoadingButton } from "@mui/lab";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: '#FDEDED',
+  boxShadow: 24,
+  borderRadius: '5px',
+};
 
 export default function Shipping() {
   const classes = useStyles();
   const router = useRouter();
   const form = useRef();
+  const [firstName, setFirstName] = useState('')
+  const [lastName, setLastName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phoneNumber, setPhoneNumber] = useState('')
+  const [description, setDescription] = useState('')
+  const [load, setLoad] = useState(false);
+  const [open, setOpen] = useState(false);
+  const handleClose = () => setOpen(false);
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs.sendForm('service_ymvfttm', 'template_lcolk1b', form.current, 'bqF2AYPOj2UR8zBaK')
-      .then((result) => {
+    setLoad(true);
+    emailjs
+      .sendForm(
+        "service_ymvfttm",
+        "template_lcolk1b",
+        form.current,
+        "bqF2AYPOj2UR8zBaK"
+      )
+      .then(
+        (result) => {
+          setOpen(true);
+          setLoad(false);
           console.log(result.text);
-      }, (error) => {
+          setFirstName('')
+          setLastName('')
+          setEmail('')
+          setPhoneNumber('')
+          setDescription('')
+        },
+        (error) => {
           console.log(error.text);
-      });
+        }
+      );
   };
 
   const continueShopping = (e) => {
     e.preventDefault();
-    router.push('/')
-  }
+    router.push("/");
+  };
   return (
     <div className={styles.shipping}>
       <Container>
         <Grid container>
           <Grid item xs={12} sm={12} md={7} lg={7}>
-            <Box component="form" ref={form} className={classes.form} onSubmit={sendEmail}>
+            <Box
+              component="form"
+              ref={form}
+              className={classes.form}
+              onSubmit={sendEmail}
+            >
               <Typography p={1}>Contact Us</Typography>
               <FormControl action="">
-                <div>                  
-                  <TextField
-                    className={classes.nameClass && styles.name}
-                    required={true}
-                    name='firstName'
-                    id="outlined-required"
-                    label="First Name"
-                    defaultValue=""
-                  />
-                  <TextField
-                    className={classes.nameClass && styles.name}
+                <div>
+                  <input
+                    className={styles.name}
                     required
+                    value={firstName}
+                    name="firstName"
+                    id="outlined-required"
+                    placeholder="First Name"
+                    defaultValue=""
+                    onChange={(e) => setFirstName(e.target.value)}
+                  />
+                  <input
+                    className={styles.name}
+                    required
+                    value={lastName}
                     name="lastName"
                     id="outlined-required"
-                    label="Last Name"
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder="Last Name"
                     defaultValue=""
                   />
-                  {/* <TextField
-                    className={classes.adress}
-                    type="select"
+                  <input
+                    className={styles.name}
                     required
-                    id="outlined-required"
-                    label="Select Country"
-                    defaultValue="Pakistan"
-                  /> */}
-                  {/* <TextField
-                    className={classes.adress}
-                    required
-                    id="outlined-required"
-                    label="Email Address"
-                    defaultValue=""
-                  /> */}
-                  <TextField
-                    className={classes.nameClass && styles.name}
-                    required
+                    value={email}
                     name="email_address"
                     id="outlined-required"
-                    label="Email Address"
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email Address"
                     defaultValue=""
                   />
-                  <TextField
-                    className={classes.nameClass && styles.name}
+                  <input
+                    className={styles.name}
                     id="outlined-required"
-                    label="Phone Number"
+                    placeholder="Phone Number"
                     name="phoneNumber"
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    value={phoneNumber}
                     defaultValue=""
                   />
                   <textarea
-                    className={classes.adress}
-                    style={{height: '100px', padding: '15px'}}
+                    className={styles.adress}
                     placeholder="What do you want to know?"
                     type="text"
+                    value={description}
                     name="description"
+                    onChange={(e) => setDescription(e.target.value)}
                     required
                     id="outlined-required"
-                    // label="What do you want to know?"
                     defaultValue=""
                   />
                 </div>
               </FormControl>
-              <Button type="submit" variant="contained" className={classes.submitbtn}>
+              {open ? (
+                <Modal
+                  open={open}
+                  onClose={handleClose}
+                  aria-labelledby="modal-modal-title"
+                  aria-describedby="modal-modal-description"
+                >
+                  <Box sx={style}>
+                    <Alert severity="success">Your Message is delivered to us. We will contact you soon.</Alert>
+                  </Box>
+                </Modal>
+              ) : (
+                ""
+              )}
+              <LoadingButton
+                loading={load}
+                type="submit"
+                variant="contained"
+                className={classes.submitbtn}
+              >
                 Submit Now
-              </Button>
-              <Button onClick={(e) => continueShopping(e)} variant="contained" className={classes.submitbtn}>
+              </LoadingButton>
+              <Button
+                onClick={(e) => continueShopping(e)}
+                variant="contained"
+                className={classes.submitbtn}
+              >
                 Continue to shoping
               </Button>
             </Box>
@@ -111,53 +179,53 @@ export default function Shipping() {
 const useStyles = makeStyles((theme) =>
   createStyles({
     form: {
-      height: 'auto',
-      width: '100%',
+      height: "auto",
+      width: "100%",
       margin: "2rem 0",
       [theme?.breakpoints?.down("sm")]: {
-        height: 'auto'
-      }
+        height: "auto",
+      },
     },
     nameClass: {
-      margin: '1rem 0.3rem',
-      width: '47.8%',
-      maxWidth: '97%',
+      margin: "1rem 0.3rem",
+      width: "47.8%",
+      maxWidth: "97%",
       [theme?.breakpoints?.up("sm")]: {
         // minWidth: '97%',
-      }
+      },
     },
     adress: {
-      margin: '0.3rem 0.3rem',
-      width: '97%',
+      margin: "0.3rem 0.3rem",
+      width: "97%",
       [theme?.breakpoints?.up("sm")]: {
-        width: '97%',
-      }
+        width: "97%",
+      },
     },
     submitbtn: {
       "&:hover": {
-        backgroundColor: '#525050',
+        backgroundColor: "#525050",
       },
-      margin: '0.5rem 0.3rem',
-      backgroundColor: '#232323',
-      color: 'white',
-      fontSize: '0.7rem'
+      margin: "0.5rem 0.3rem",
+      backgroundColor: "#232323",
+      color: "white",
+      fontSize: "0.7rem",
     },
-    total:{
-      display: 'flex',
-      justifyContent: 'space-between',
-      margin: '6rem 1rem 0 2rem',
-      color: 'rgb(110, 106, 106)',
+    total: {
+      display: "flex",
+      justifyContent: "space-between",
+      margin: "6rem 1rem 0 2rem",
+      color: "rgb(110, 106, 106)",
     },
     ship: {
-      display: 'flex',
-      justifyContent: 'space-between',
-      margin: '1rem 1rem 0 2rem',
-      color: 'rgb(104, 101, 101)',
+      display: "flex",
+      justifyContent: "space-between",
+      margin: "1rem 1rem 0 2rem",
+      color: "rgb(104, 101, 101)",
     },
     hr: {
-      width: '93%',
-      margin: 'auto',
-      color: 'lightgrey',
-    }
+      width: "93%",
+      margin: "auto",
+      color: "lightgrey",
+    },
   })
 );
